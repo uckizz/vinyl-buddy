1701c6901e8d5b5654943503cd8107cb2523c412
uckizz <nicklas.thirus@gmail.com>
2019-12-10 13:08:15 +0100

    Small fixes

--- src/components/TrackInfo.js
+++ src/components/TrackInfo.js
@@ -6,7 +6,7 @@ export default function TrackInfo(props) {
       <h3>Artist: {props.trackInfo.artist}</h3>
       <h3>Album: {props.trackInfo.album}</h3>
       <h3>Song: {props.trackInfo.song}</h3>
-      <img src={props.trackInfo.cover}></img>
+      <img className="cover-image" src={props.trackInfo.cover}></img>
     </div>
   );
 }
