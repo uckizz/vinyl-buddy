2190706ef93fcf5d1f50a6d5db8dd8b29d5ff8f7
uckizz <nicklas.thirus@gmail.com>
2019-12-10 15:18:19 +0100

    Style fixes

--- src/components/TrackInfo.js
+++ src/components/TrackInfo.js
@@ -3,14 +3,15 @@ import React from "react";
 export default function TrackInfo(props) {
   return (
     <div className="track-info">
-      <h3>Artist: {props.trackInfo.artist}</h3>
-      <h3>Album: {props.trackInfo.album}</h3>
-      <h3>Song: {props.trackInfo.song}</h3>
       <img
         className="cover-image"
         src={props.trackInfo.cover}
         alt="album-cover"
       ></img>
+      <h3>
+        {props.trackInfo.artist} - {props.trackInfo.song}
+      </h3>
+      <h3>{props.trackInfo.album}</h3>
     </div>
   );
 }
